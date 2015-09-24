d02c5b9dcf01951ccca03e347ac5004bd3dac676
kwhite8933 <kwhite8933@gmail.com>
2015-09-24 01:24:08 -0400

    Added AddAnExpense Class and Button Handler for Add an Expense Activity

--- app/src/main/java/com/kyle/moneymanagement/AddAnExpense.java
+++ app/src/main/java/com/kyle/moneymanagement/AddAnExpense.java
@@ -1,37 +1,24 @@
 package com.kyle.moneymanagement;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class AddAnExpense extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button add_expense_button = (Button) findViewById(R.id.add_an_expense);
-        add_expense_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent( MainActivity.this, AddAnExpense.class );
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_add_an_expense);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_add_an_expense, menu);
         return true;
     }
 
@@ -43,9 +30,9 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        //if (id == R.id.action_settings) {
-        //    return true;
-        //}
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
